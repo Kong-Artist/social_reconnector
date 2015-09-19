0281a1ccd171d2968a938c21ae1d19395d0b2009
Pritham Marupaka <mpritham@users.noreply.github.com>
2015-09-19 15:27:56 -0400

    Get friends and posts

--- web/app.py
+++ web/app.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template
+import pdb
+import requests
+import json
+
+ACCESS = "CAACEdEose0cBAHqCZALqKKadWZCoT563wLc2LO3VqzHNrjG9GucmlBtMBG6iHBjZCzFPAjZCK8RZA280qiKzcJrmZBZA0Q9YaPHEmYV0UeXrfVXfdCnRSxCVTWR3HNbcFnMeCRQloNEZB4ItWgk3jyhHaYHMAMZCDHZAkzVFaZBRIOKWGuZCr5yFKaAeLFyjlqQZBC9PZBPfJH99bx2EqIQMxJMbW2"
+
+ROOT_URL = "https://graph.facebook.com/v2.4/"
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    friends = get_friends()
+    return render_template("index.html", friends=friends["data"])
+
+def get_friends(access_token=ACCESS, user="me"):
+    data = requests.get(ROOT_URL + user + "friends?access_token=" + access_token)
+    return json.loads(data.text)
+
+def get_posts(access_token=ACCESS, user="me"):
+    data = requests.get(ROOT_URL + user + "/posts?access_token=" + access_token)
+    return json.loads(data.text)
 
 if __name__ == "__main__":
     app.run(debug=True)
